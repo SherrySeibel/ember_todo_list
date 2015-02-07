eebc12bbe379a671a774e2721685f29dc5875e0e
Sherry Seibel <sherry.seibel@gmail.com>
2015-02-07 17:56:30 -0500

    Remove a todo from the list when the 'X' button is clicked

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -7,6 +7,18 @@ export default Ember.ObjectController.extend({
     },
     acceptChanges: function() {
       this.set("isEditing", false);
+
+      if(Ember.isEmpty(this.get("model.title"))) {
+        this.send("removeTodo");
+      } else {
+        this.get("model").save();
+      }
+    },
+
+    removeTodo: function() {
+      var todo = this.get("model");
+      todo.deleteRecord();
+      todo.save();
     }
   }
 });
