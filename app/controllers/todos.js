ce2714421cb5d58a9f9423e7b7be2d07d918e18a
Sherry Seibel <sherry.seibel@gmail.com>
2015-02-07 22:12:27 -0500

    Check the box when all the todos are completed

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -8,8 +8,14 @@ export default Ember.ArrayController.extend({
         isCompleted: false
       });
 
-      this.set("newTodo", "");
-      this.save();
+      this.set("newTitle", "");
+      todo.save();
+    },
+
+    clearCompleted: function() {
+      var completed = this.filterBy("isCompleted", true);
+      completed.invoke("deleteRecord");
+      completed.invoke("save");
     }
   },
 
@@ -20,5 +26,23 @@ export default Ember.ArrayController.extend({
   inflection: function() {
     var remaining = this.get("remaining");
     return (remaining === 1) ? "item" : "items";
-  }.property("remaining")
+  }.property("remaining"),
+
+  hasCompleted: function() {
+    return this.get("completed") > 0;
+  }.property("completed"),
+
+  completed: function() {
+    return this.filterBy("isCompleted", true).get("length");
+  }.property("@each.isCompleted"),
+
+  allAreDone: function(key, value) {
+    if (value === undefined) {
+      return this.get("length") > 0 && this.isEvery("isCompleted", true);
+    } else {
+      this.setEach("isCompleted", value);
+      this.invoke("save");
+      return value;
+    }
+  }.property("@each.isCompleted")
 });
